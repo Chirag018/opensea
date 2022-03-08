2cecd6db42327c31bda1010521b85e5b23650475
Chirag <56735482+Chirag018@users.noreply.github.com>
2022-03-09 01:19:05 +0530

    refactoring

--- src/components/Main/Categories.js
+++ src/components/Main/Categories.js
@@ -7,6 +7,7 @@ import exp5 from '../../assets/exp5.png'
 import exp6 from '../../assets/exp6.png'
 import exp7 from '../../assets/exp7.png'
 import exp8 from '../../assets/exp8.png'
+import Categories_repeator from '../../data/Categories_repeator'
 
 function Categories() {
     return (
@@ -17,54 +18,50 @@ function Categories() {
                 <div className="cols lg:grid lg:grid-cols-4 lg:gap-x-48 lg:gay-y-48 lg:space-y-1 lg:pr-20 space-y-4">
                     <div className="art flex flex-row w-64 h-24 text-left gap-x-2 gap-y-10">
                         <img src={exp1} alt="" className="" />
-                        <div>
-                            <h1 className="text-blue-700 ">Art</h1>
-                            <span>Art is something that stimulates an individual’s thoughts</span>
-                        </div>
+                        <Categories_repeator title={'Art'} para={'Art is something that stimulates an individual’s thoughts'} />
+
                     </div>
                     <div className="music flex flex-row w-64 h-24 text-left gap-x-2">
                         <img src={exp2} alt="" class="" />
-                        <div>
-                            <h1 class="text-blue-700">Music</h1>
-                            <span>Music is the art of arraning sounds elements of melody</span>
-                        </div>
+                        <Categories_repeator title={'Music'} para={'Music is the art of arraning sounds elements of melody'} />
+
                     </div>
                     <div className="vw flex flex-row w-64 h-24 text-left gap-x-2">
                         <img src={exp3} alt="" class="" />
-                        <div>
-                            <h1 className="text-blue-700">Visual Worlds</h1>
-                            <span>Visuals are the picture elements , as distinguished</span>
-                        </div>
-                    </div><div className="art flex flex-row w-64 h-24 text-left gap-x-2">
+                        <Categories_repeator title={'Visual Worlds'} para={'Visuals are the picture elements , as distinguished'} />
+
+
+                    </div>
+                    <div className="art flex flex-row w-64 h-24 text-left gap-x-2">
                         <img src={exp4} alt="" className="" />
+                        <Categories_repeator title={'Trading Cards'} para={'A Trading card ( or collectible card ) is a small card.'} />
+
                         <div>
-                            <h1 className="text-blue-700">Trading Cards</h1>
-                            <span>A Trading card ( or collectible card ) is a small card.</span>
+                            <h1 className="text-blue-700"></h1>
+                            <span></span>
                         </div>
-                    </div><div className="art flex flex-row w-64 h-24 text-left gap-x-2">
+                    </div>
+                    <div className="art flex flex-row w-64 h-24 text-left gap-x-2">
                         <img src={exp5} alt="" class="" />
-                        <div>
-                            <h1 className="text-blue-700">Collectibles</h1>
-                            <span>An item worth far more than it was sold for rarity.</span>
-                        </div>
-                    </div><div className="art flex flex-row w-64 h-24 text-left gap-x-2">
+                        <Categories_repeator title={'Collectibles'} para={'An item worth far more than it was sold for rarity.'} />
+
+
+                    </div>
+                    <div className="art flex flex-row w-64 h-24 text-left gap-x-2">
                         <img src={exp6} alt="" class="" />
-                        <div>
-                            <h1 className="text-blue-700">Sports</h1>
-                            <span>Sport pertains to any form of competitive.</span>
-                        </div>
-                    </div><div className="art flex flex-row w-64 h-24 text-left gap-x-2">
+                        <Categories_repeator title={'Sports'} para={'Sport pertains to any form of competitive.'} />
+
+                    </div>
+                    <div className="art flex flex-row w-64 h-24 text-left gap-x-2">
                         <img src={exp7} alt="" className="" />
-                        <div>
-                            <h1 className="text-blue-700">Domain Name</h1>
-                            <span>Use Traditional Domains. The native name suffix.</span>
-                        </div>
-                    </div><div class="art flex flex-row w-64 h-24 text-left gap-x-2">
+                        <Categories_repeator title={'Domain Name'} para={'Use Traditional Domains. The native name suffix.'} />
+
+                    </div>
+                    <div class="art flex flex-row w-64 h-24 text-left gap-x-2">
                         <img src={exp8} alt="" className="" />
-                        <div>
-                            <h1 className="text-blue-700">All NFT’s</h1>
-                            <span>Music is the art of arranging sounds elements of melody</span>
-                        </div>
+                        <Categories_repeator title={'All NFT’s'} para={'Music is the art of arranging sounds elements of melody'} />
+
+
                     </div>
 
                 </div>
@@ -78,4 +75,4 @@ function Categories() {
 
 export default Categories
 
-//code to be refactored .
+//code to be refactored .(done)
---
+++ src/data/Categories_repeator.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Categories_repeator({title,para}) {
+    return (
+            <div>
+                <h1 class="text-blue-700">{title}</h1>
+                <span>{para}</span>
+            </div>
+    )
+}
+
+export default Categories_repeator
