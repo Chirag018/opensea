52b91b19e08a37f0288fada77656286e6acdfcdb
Chirag <56735482+Chirag018@users.noreply.github.com>
2022-03-08 15:04:32 +0530

    deploy

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
-import Footer from './components/Footer';
-import Header from './components/Header';
-import Main from './components/Main';
+import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
+import Main from './components/Main/Main';
 
 function App() {
   return (
     <div className="App">
-      <Header />{/* media query to be .. */}
+      <Header />{/* media query done☹️ .. */}
       <Main />{/* media query done  */}
       <Footer /> {/* media query done */}
     </div>
